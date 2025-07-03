80c5b42b4211e050cc9e36e06497a71c136632d4
Emiryasin251 <110299247+Emiryasin251@users.noreply.github.com>
2025-07-03 15:59:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//document.write(); çıktı verme
+//console.log("Hello world"); konsola çıktı verme
+alert("ŞEVVAL ADAMDIR!!!");
+//console.log(window);
+
+let ilkSayi = prompt("ilk sayıyı giriniz: ");
+let ikinciSayi = prompt("ikinci sayıyı giriniz: ");
+let ucuncuciSayi = prompt("üçüncü sayıyı giriniz: ");
+
+console.log(ilkSayi + ikinciSayi + ucuncuciSayi);
